93cb74a041c293880d5e8e10c03310f1b6a7ba9c
Brandon Chen <bchen00@stuy.edu>
2019-03-23 12:51:37 -0400

    working on add and resize

--- MyDeque.java
+++ MyDeque.java
@@ -1,21 +1,29 @@
 import java.util.NoSuchElementException;
 public class MyDeque<E>{
   private E[] data;
-  private int size, start, end;
+  private int size = 0;
+  private int start = 0;
+  private int end = 0;
 
   @SuppressWarnings("unchecked")
   public MyDeque(){
-    size = 0;
-    start = 0;
-    end = 9;
+  //  @SuppressWarnings("unchecked")
     data = (E[])new Object[10];
+    //size = 0;
+    //start = 0;
+    //end = 0;
+    //@SuppressWarnings("unchecked")
+    //data = (E[])new Object[10];
   }
   @SuppressWarnings("unchecked")
   public MyDeque(int initialCapacity){
-    size = 0;
-    start = 0;
-    end = initialCapacity-1;
+  //  @SuppressWarnings("unchecked")
     data = (E[])new Object[initialCapacity];
+    //size = 0;
+    //start = 0;
+    //end = 0;
+    //@SuppressWarnings("unchecked")
+    //data = (E[])new Object[initialCapacity];
   }
   public int size(){
     return size;
@@ -32,11 +40,17 @@ public class MyDeque<E>{
     if (element == null){
       throw new NullPointerException("boo");
     }
-    if (start - 1 >= 0){
+    if (size == data.length){
+      resize();
+    }
+    if (start < 0){
+      start = data.length - 1;
+    }
+    //if (start - 1 >= 0){
       start--;
       size++;
       data[start] = element;
-    }
+    //}
   }
   public void addLast(E element){
     if (element == null){
@@ -48,6 +62,8 @@ public class MyDeque<E>{
       data[end] = element;
     }
   }
+
+
   public E removeFirst(){
     if (data[start] == null){
       throw new NoSuchElementException("boo");
@@ -77,5 +93,22 @@ public class MyDeque<E>{
       throw new NoSuchElementException("boo");
     }
     return data[end];
+  }
+
+  //@SuppressWarnings("unchecked")
+  private void resize(){
+    //@SuppressWarnings("unchecked")
+    int newSize = data.length * 2 + 1;
+    @SuppressWarnings("unchecked")
+    E[] newData = (E[])new Object[newSize];
+    for (int i = 0; i < size; i++){
+      newData[i] = data[i];
+    }
+    data = newData;
+  }
+  @SuppressWarnings("unchecked")
+  public static void main(String args[]){
+    MyDeque data = new MyDeque();
+    data.addFirst(10);
   }
 }
