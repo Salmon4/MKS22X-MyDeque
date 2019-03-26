465ff1dbf16cfaf2a6324cbdd6c2db81e2a4b4e8
Brandon <bchen00@stuy.edu>
2019-03-26 09:44:38 -0400

    split string into an array separated by space

---
+++ Calculator.java
@@ -0,0 +1,16 @@
+public class Calculator{
+  /*Evaluate a postfix expression stored in s.
+ *Assume valid postfix notation, separated by spaces.
+ */
+ public static double eval(String s){
+   String[] stack = s.split(" ");
+   //for (int i = 0; i < stack.length; i++){
+   //System.out.println(stack[i]);
+ //}
+  return .01;
+ }
+
+ public static void main(String args[]){
+   System.out.println(eval("w o r k s"));
+ }
+}
