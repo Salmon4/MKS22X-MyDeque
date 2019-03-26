7251d096bb9316391c8bfb36bcb9d626f12d60b6
Brandon <bchen00@stuy.edu>
2019-03-26 10:17:42 -0400

    wrote most of eva but bad types operators dont work

--- Calculator.java
+++ Calculator.java
@@ -2,12 +2,45 @@ public class Calculator{
   /*Evaluate a postfix expression stored in s.
  *Assume valid postfix notation, separated by spaces.
  */
+ @SuppressWarnings("unchecked")
  public static double eval(String s){
-   String[] stack = s.split(" ");
+   String[] stackS = s.split(" ");
    //for (int i = 0; i < stack.length; i++){
    //System.out.println(stack[i]);
  //}
-  return .01;
+  MyDeque numb = new MyDeque();
+  MyDeque opera = new MyDeque();
+  for (int i = 0; i < stackS.length; i++){
+    if (stackS[i] == "+"){
+      opera.addLast(stackS[i]);
+    }
+    if (stackS[i] == "-"){
+      opera.addLast(stackS[i]);
+    }
+    if (stackS[i] == "*"){
+      opera.addLast(stackS[i]);
+    }
+    if (stackS[i] == "/"){
+      opera.addLast(stackS[i]);
+    }
+    numb.addLast(stackS[i]);
+  }
+  for (int i = 0; numb.size() != 1; i++){
+    if (opera.removeFirst().equals("+")){
+      numb.addFirst(numb.removeFirst() + numb.removeFirst());
+    }
+    if (opera.removeFirst() == "-"){
+      numb.addFirst(numb.removeFirst() - numb.removeFirst());
+    }
+    if (opera.removeFirst() == "*"){
+      numb.addFirst(numb.removeFirst() * numb.removeFirst());
+    }
+    if (opera.removeFirst() == "/"){
+      numb.addFirst(numb.removeFirst() / numb.removeFirst());
+    }
+    i--;
+  }
+  return numb.getFirst();
  }
 
  public static void main(String args[]){
