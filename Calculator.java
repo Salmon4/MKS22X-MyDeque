28f5dbff66062303df36bcdaebda3231841eab7d
Brandon Chen <bchen00@stuy.edu>
2019-03-26 21:22:13 -0400

    rewrote it to the proper way i think

--- Calculator.java
+++ Calculator.java
@@ -4,41 +4,40 @@ public class Calculator{
  */
  @SuppressWarnings("unchecked")
  public static double eval(String s){
-   String[] stackS = s.split(" ");
+   String[] stack = s.split(" ");
    //for (int i = 0; i < stack.length; i++){
    //System.out.println(stack[i]);
  //}
-  MyDeque numb = new MyDeque();
-  MyDeque opera = new MyDeque();
-  for (int i = 0; i < stackS.length; i++){
-    if (stackS[i] == "+"){
-      opera.addLast(stackS[i]);
+  MyDeque<Double> numb = new MyDeque();
+  //MyDeque<Double> opera = new MyDeque();
+
+  for (int i = 0; i < stack.length; i++){
+    if (stack[i] == "+"){
+      numb.addLast(numb.removeLast() + numb.removeLast());
     }
-    if (stackS[i] == "-"){
-      opera.addLast(stackS[i]);
+    else{
+      if (stack[i] == "-"){
+        numb.addLast(numb.removeLast() - numb.removeLast());
+      }
+      else{
+        if (stack[i] == "*"){
+          numb.addLast(numb.removeLast() * numb.removeLast());
+        }
+        else{
+          if (stack[i] == "/"){
+            numb.addLast(numb.removeLast() / numb.removeLast());
+          }
+          else{
+            if (stack[i] == "%"){
+              numb.addLast(numb.removeLast() % numb.removeLast());
+            }
+            else{
+              numb.addLast((double)stack[i]);
+            }
+          }
+        }
+      }
     }
-    if (stackS[i] == "*"){
-      opera.addLast(stackS[i]);
-    }
-    if (stackS[i] == "/"){
-      opera.addLast(stackS[i]);
-    }
-    numb.addLast(stackS[i]);
-  }
-  for (int i = 0; numb.size() != 1; i++){
-    if (opera.removeFirst().equals("+")){
-      numb.addFirst(numb.removeFirst() + numb.removeFirst());
-    }
-    if (opera.removeFirst() == "-"){
-      numb.addFirst(numb.removeFirst() - numb.removeFirst());
-    }
-    if (opera.removeFirst() == "*"){
-      numb.addFirst(numb.removeFirst() * numb.removeFirst());
-    }
-    if (opera.removeFirst() == "/"){
-      numb.addFirst(numb.removeFirst() / numb.removeFirst());
-    }
-    i--;
   }
   return numb.getFirst();
  }
